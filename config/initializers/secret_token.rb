9d38d284ee29c58278f4a83930a85720401647b6
Shannon Rush <shannonmrush@gmail.com>
2013-07-03 20:17:58 -0600

    skeleton rails 4

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Followmap::Application.config.secret_key_base = 'a1a5fabd7b296e352e4ed23249d8c24a875d573a7c8ba90fd51f484461c337388515420bf2a59119c9c8ae70fa05f2f8716d26b8e3c668c7541d55875c122f68'
